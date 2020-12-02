016f96483258f51b62d97f8b8dcad21f17670643
SeojinSeojin <1106laura@naver.com>
2020-12-02 10:34:33 +0900

    [feat] POST 요청 추가

---
+++ backtrack_victory.py
@@ -0,0 +1,3 @@
+def get_victory_percentage(cards):
+    print(cards)
+    return 0
--- init.py
+++ init.py
@@ -1,11 +1,26 @@
-from flask import Flask, render_template
-app = Flask(__name__)       
- 
+from flask import Flask, render_template, request, redirect
+from backtrack_victory import get_victory_percentage
+app = Flask(__name__)
 
-@app.route("/")              
 
+@app.route("/")
 def home():
-    return render_template("main.html")              
+    return render_template("main.html")
 
-if __name__ == "__main__": 
+
+@app.route("/postCard", methods=['POST'])
+def post():
+    result = []
+    for key, value in request.form.items():
+        # print(key, value)  # 입력받은 값들 확인
+        result.append(value)
+    print(result)
+    if " " in result:
+        return redirect("/")
+    else:
+        vp = get_victory_percentage(result)
+        return render_template("result.html", vp=vp)
+
+
+if __name__ == "__main__":
     app.run()
